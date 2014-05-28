c11f21d6d3306750b535017a92fd0c206504f763
Allan Jhones Ribeiro de Souza <allansouza@MacBook-Pro-de-Allan.local>
2014-05-28 14:42:45 -0300

    Initial Commit

---
+++ QQISSO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QQISSO
+//
+//  Created by Allan Jhones Ribeiro de Souza on 28/05/14.
+//  Copyright (c) 2014 Allan Jhones Ribeiro de Souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ QQISSOTests/QQISSOTests.m
@@ -0,0 +1,34 @@
+//
+//  QQISSOTests.m
+//  QQISSOTests
+//
+//  Created by Allan Jhones Ribeiro de Souza on 28/05/14.
+//  Copyright (c) 2014 Allan Jhones Ribeiro de Souza. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QQISSOTests : XCTestCase
+
+@end
+
+@implementation QQISSOTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
