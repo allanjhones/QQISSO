090565f46e1bf8ce2fe9fd346796522c930e10d7
Allan Jhones Ribeiro de Souza <allansouza@MacBook-Pro-de-Allan.local>
2014-05-28 15:05:41 -0300

    adicionado tela inicial do jogo

---
+++ QQISSO/TelaInicialViewController.h
@@ -0,0 +1,13 @@
+//
+//  TelaInicialViewController.h
+//  QQISSO
+//
+//  Created by Allan Jhones Ribeiro de Souza on 28/05/14.
+//  Copyright (c) 2014 Allan Jhones Ribeiro de Souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TelaInicialViewController : UIViewController
+
+@end
---
+++ QQISSO/TelaInicialViewController.m
@@ -0,0 +1,38 @@
+//
+//  TelaInicialViewController.m
+//  QQISSO
+//
+//  Created by Allan Jhones Ribeiro de Souza on 28/05/14.
+//  Copyright (c) 2014 Allan Jhones Ribeiro de Souza. All rights reserved.
+//
+
+#import "TelaInicialViewController.h"
+
+@interface TelaInicialViewController ()
+
+@end
+
+@implementation TelaInicialViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
